3c90b340f01213f8f60363b9f92da731a26ebe37
Christian Macdonald <cmacdonald@A11073.hitronhub.home>
2021-01-11 08:16:45 -0700

    updated customer cancellation file

--- AthlosSpedDatabase/CommonQueries/UserCreation/CustomerCancellation.sql
+++ AthlosSpedDatabase/CommonQueries/UserCreation/CustomerCancellation.sql
@@ -1,12 +1,29 @@
 SELECT * FROM Customers;
 GO
 
+-- UDPATE CANCELLED CUSTOMERS TO REMOVE ACCESS
+
 UPDATE dbo.Customers
 SET CustomerIsCurrent = 0
-WHERE CustomerID = 60;
+WHERE CustomerID = 70;
 GO
 
 UPDATE dbo.Customers
-SET CustomerEndDate = '2021-01-02'
-WHERE CustomerID = 60;
+SET CustomerEndDate = '2021-01-09'
+WHERE CustomerID = 70;
+GO
+
+-- DELETE TEST CUSTOMERS, DISTRICTS, AND SCHOOLS
+
+DELETE FROM School WHERE DistrictID IN (
+    SELECT DistrictID FROM District WHERE CustomerID IN (
+        SELECT CustomerID FROM Customers WHERE CustomerContactEmail LIKE '%test%')
+        );
+GO
+
+DELETE FROM District WHERE CustomerID IN (
+    SELECT CustomerID FROM Customers WHERE CustomerContactEmail LIKE '%test%');
+GO
+
+DELETE FROM Customers WHERE CustomerContactEmail LIKE '%test%';
 GO
