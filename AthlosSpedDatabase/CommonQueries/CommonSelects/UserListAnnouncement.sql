76160ac5d9127728ef5de4d1bf90eab81c014136
Christian Macdonald <cmacdonald@A11073.hitronhub.home>
2021-01-11 12:30:52 -0700

    individual customer query

--- AthlosSpedDatabase/CommonQueries/CommonSelects/UserListAnnouncement.sql
+++ AthlosSpedDatabase/CommonQueries/CommonSelects/UserListAnnouncement.sql
@@ -19,3 +19,25 @@ ORDER BY
 		ELSE 3
 	END;
 GO
+
+-- SELECT ALL INDIVIDUAL CUSTOMERS
+
+SELECT
+	[Name] = U.UserFirstName + ' ' + U.UserLastName
+	,U.UserEmail
+	,U.UserRole
+	,D.DistrictName
+	,C.CustomerLicenseType
+FROM dbo.Users AS U
+INNER JOIN dbo.District AS D ON D.DistrictID = U.DistrictID
+INNER JOIN dbo.Customers AS C ON C.CustomerID = D.CustomerID
+WHERE C.CustomerIsCurrent = 1 AND U.DistrictID <> 1 AND U.DistrictID <> 7 AND (U.UserIsCurrent = 1 OR AspNetUserID IS NULL) AND CustomerLicenseType = 'Individual'
+ORDER BY
+	C.CustomerLicenseType
+	,D.DistrictName
+	,CASE
+		WHEN U.UserRole = 'disadmin' THEN 1
+		WHEN U.UserRole = 'schoadmin' THEN 2
+		ELSE 3
+	END;
+GO
