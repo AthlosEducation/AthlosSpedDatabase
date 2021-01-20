c89bc348b16b9d92d4a3886ce3210444ee8ab13a
Christian Macdonald <cmacdonald@A11073.hitronhub.home>
2021-01-20 06:45:47 -0700

    update sync check

--- AthlosSpedDatabase/CommonQueries/SyncCheck/SyncCheck_GoalMap.sql
+++ AthlosSpedDatabase/CommonQueries/SyncCheck/SyncCheck_GoalMap.sql
@@ -13,16 +13,12 @@ SELECT * FROM GoalServiceAreaMapping;
 -- Insert list of new goal names into the Goal Service Area Mapping Table
 INSERT INTO dbo.GoalServiceAreaMapping (District, GoalCategory, GoalServiceAreaID)
 VALUES 
-	('Athlos Academies','Functional Skills',18)
-	,('Atwater Elementary School District','Communication    Comunicacion',12)
-	,('Atwater Elementary School District','Decoding Words With 3-4 Phonemes',12)
-	,('Atwater Elementary School District','Decoding Words With Consonant Blends',12)
-	,('Atwater Elementary School District','Numbers & Operations In Base Ten-Add/Subtract',2)
-	,('Atwater Elementary School District','Numbers & Operations In Base Ten-Place Value',2)
-	,('Atwater Elementary School District','Numbers & Operations-Fractions',2)
-	,('Atwater Elementary School District','Ratios And Proportional Relationships',2)
-	,('Atwater Elementary School District','Solving Double Digit Addition/Subtraction Problems',2)
-	,('Atwater Elementary School District','Task Completion   Completar Tareas',10);
+	('Athlos Academies','Aggression/Meltdowns',11)
+	,('Athlos Academies','Theater',7)
+	,('Atwater Elementary School District','Math Number Sense',2)
+	,('Atwater Elementary School District','Oral Comprehension',12)
+	,('Atwater Elementary School District','Organization/ Work Completion',10)
+	,('Minnesota Internship Center','Organizational Skills',10);
 GO
 
 -- Remove from no match list
