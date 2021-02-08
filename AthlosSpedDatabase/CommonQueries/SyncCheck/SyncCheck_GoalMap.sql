c4874fc523ff0da7510235acab4b094d88060a94
Christian Macdonald <cmacdonald@A11073.local>
2021-02-08 07:54:01 -0700

    viewstudent history allowed

--- AthlosSpedDatabase/CommonQueries/SyncCheck/SyncCheck_GoalMap.sql
+++ AthlosSpedDatabase/CommonQueries/SyncCheck/SyncCheck_GoalMap.sql
@@ -13,12 +13,12 @@ SELECT * FROM GoalServiceAreaMapping;
 -- Insert list of new goal names into the Goal Service Area Mapping Table
 INSERT INTO dbo.GoalServiceAreaMapping (District, GoalCategory, GoalServiceAreaID)
 VALUES 
-	('Athlos Academies','Aggression/Meltdowns',11)
-	,('Athlos Academies','Theater',7)
-	,('Atwater Elementary School District','Math Number Sense',2)
-	,('Atwater Elementary School District','Oral Comprehension',12)
-	,('Atwater Elementary School District','Organization/ Work Completion',10)
-	,('Minnesota Internship Center','Organizational Skills',10);
+	('','',3)
+	,('','',12)
+	,('','',2)
+	,('','',2)
+	,('','',3)
+	,('','',3);
 GO
 
 -- Remove from no match list
