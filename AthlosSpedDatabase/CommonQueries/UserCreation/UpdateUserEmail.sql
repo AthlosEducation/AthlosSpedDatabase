ab925f2fdc1e7dbc5de103657727e6955cf12e6c
Christian Macdonald <cmacdonald@A11073.local>
2021-03-04 07:58:07 -0700

    updated user email file

--- AthlosSpedDatabase/CommonQueries/UserCreation/UpdateUserEmail.sql
+++ AthlosSpedDatabase/CommonQueries/UserCreation/UpdateUserEmail.sql
@@ -1,25 +1,23 @@
-SELECT * FROM Users;
-GO
-
-SELECT * FROM AspNetUsers WHERE Id = '8f37fb80-4b68-43c2-b831-5d750dca620a';
-GO
-
-UPDATE AspNetUsers
-SET UserName = 'nachole.stewart@student.dodea.edu' 
-WHERE Id = '8f37fb80-4b68-43c2-b831-5d750dca620a';
+SELECT * FROM Users WHERE UserID = 470;
+SELECT * FROM AspNetUsers WHERE Id = 'e2674adb-c2d1-4c81-8572-36636857d424';
 GO
 
 UPDATE AspNetUsers
-SET Email = 'nachole.stewart@student.dodea.edu' 
-WHERE Id = '8f37fb80-4b68-43c2-b831-5d750dca620a';
+SET UserName = 'djohn@northsidechs.org' 
+WHERE Id = 'e2674adb-c2d1-4c81-8572-36636857d424';
 GO
 
 UPDATE AspNetUsers
-SET NormalizedUserName = 'NACHOLE.STEWART.@STUDENT.DODEA.EDU' 
-WHERE Id = '8f37fb80-4b68-43c2-b831-5d750dca620a';
+SET Email = 'djohn@northsidechs.org' 
+WHERE Id = 'e2674adb-c2d1-4c81-8572-36636857d424';
 GO
 
 UPDATE AspNetUsers
-SET NormalizedEmail = 'NACHOLE.STEWART.@STUDENT.DODEA.EDU' 
-WHERE Id = '8f37fb80-4b68-43c2-b831-5d750dca620a';
+SET NormalizedUserName = 'DJOHN@NORTHSIDECHS.ORG' 
+WHERE Id = 'e2674adb-c2d1-4c81-8572-36636857d424';
+GO
+
+UPDATE AspNetUsers
+SET NormalizedEmail = 'DJOHN@NORTHSIDECHS.ORG' 
+WHERE Id = 'e2674adb-c2d1-4c81-8572-36636857d424';
 GO
